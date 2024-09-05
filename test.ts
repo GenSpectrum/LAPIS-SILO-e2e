a28c443f754fbc77ac3c790da1b35fc93e84ceb6
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-09-05 11:02:47 +0200

    refactor: some small fixes

--- test.ts
+++ test.ts
@@ -7,11 +7,8 @@ import { randomBytes } from 'node:crypto';
 const PORT_BASE = 8000;
 
 // A salt for docker compose project names such that they do not conflict with previous or parallel test runs
-const docker_project_salt = randomBytes(Math.ceil(2))
-    .toString('hex') // Convert bytes to hex string
-    .slice(0, 4);
+const docker_project_salt = randomBytes(Math.ceil(2)).toString('hex').slice(0, 4);
 
-// Helper function to get all test set directories
 function getTestsetDirectories() {
     const testsetsPath = path.resolve(__dirname, 'testsets');
     return fs
@@ -28,11 +25,9 @@ const testsets: [string, number, number][] = getTestsetDirectories().map((dir, i
 
 console.log('All found testsets with corresponding LAPIS and SILO port numbers:', testsets);
 
-// Iterate over each testset
 testsets.forEach(([testsetDir, lapisPort, siloPort]) => {
     const testBaseDir = path.basename(testsetDir);
     const testName = (testBaseDir + '_' + docker_project_salt).toLowerCase();
-    console.log(`Hello testset: ${testBaseDir} (${testName}).`);
 
     const dataDir = path.join(testsetDir, 'data');
     const queriesDir = path.join(testsetDir, 'queries');
@@ -40,36 +35,29 @@ testsets.forEach(([testsetDir, lapisPort, siloPort]) => {
     if (!fs.existsSync(outputDir)) {
         fs.mkdirSync(outputDir);
     }
-    const dockerComposeFile = 'docker-compose.yml';
-
     const dockerComposeEnv = `LAPIS_TAG=latest SILO_TAG=latest \
         LAPIS_PORT=${lapisPort} SILO_PORT=${siloPort} \
         TESTSET_DATA_FOLDER=${dataDir} TESTSET_OUTPUT_FOLDER=${outputDir}`;
 
     describe(`Testset: ${testName}`, () => {
         beforeAll(() => {
-            if (fs.existsSync(dockerComposeFile)) {
-                console.log(`Starting Docker Compose for ${testsetDir}...`);
+            console.log(`Starting Docker Compose for ${testsetDir}...`);
 
-                const dockerComposeUpCommand = `${dockerComposeEnv} BUILDKIT_PROGRESS=plain docker compose -p ${testName} up --no-recreate -d --wait`;
+            const dockerComposeUpCommand = `${dockerComposeEnv} BUILDKIT_PROGRESS=plain docker compose --project-name ${testName} up --no-recreate --detach --wait`;
 
-                console.log(dockerComposeUpCommand);
-                execSync(dockerComposeUpCommand, { stdio: 'inherit' });
-            }
+            console.log(dockerComposeUpCommand);
+            execSync(dockerComposeUpCommand, { stdio: 'inherit' });
         });
 
         afterAll(() => {
-            if (fs.existsSync(dockerComposeFile)) {
-                console.log(`Stopping Docker Compose for ${testsetDir}...`);
+            console.log(`Stopping Docker Compose for ${testsetDir}...`);
 
-                const dockerComposeDownCommand = `docker compose -p ${testName} down && sleep 1`;
+            const dockerComposeDownCommand = `docker compose -p ${testName} down && sleep 1`;
 
-                console.log(dockerComposeDownCommand);
-                execSync(dockerComposeDownCommand, { stdio: 'inherit' });
-            }
+            console.log(dockerComposeDownCommand);
+            execSync(dockerComposeDownCommand, { stdio: 'inherit' });
         });
 
-        // Load and execute each test case from the queries folder
         fs.readdirSync(queriesDir).forEach((file) => {
             if (!file.endsWith('.query.json')) {
                 return;
@@ -79,11 +67,10 @@ testsets.forEach(([testsetDir, lapisPort, siloPort]) => {
 
             it(`should validate query from ${file}`, async () => {
                 if (!testCase.name || !testCase.endpoint || !testCase.method || !testCase.headers || !testCase.body) {
-                    console.log(`The query '${file}' must contain name, endpoint, method, headers and body`);
-                    expect(false);
+                    expect.fail(`The query '${file}' must contain name, endpoint, method, headers and body`);
                 }
 
-                const url = path.join('http://localhost:' + lapisPort, testCase.endpoint);
+                const url = `http://localhost:${lapisPort}${testCase.endpoint}`;
 
                 const response = await fetch(url, {
                     method: testCase.method,
