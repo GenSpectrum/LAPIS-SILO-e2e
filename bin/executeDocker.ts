e58c123e759b57d4ae6efa4411e725861069f9e3
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-10-21 14:25:41 +0200

    refactor: generalize and unify docker scripts

---
+++ bin/executeDocker.ts
@@ -0,0 +1,26 @@
+import { getTestSets, TestSet } from '../src/testSet.ts';
+import { dockerComposeDown, dockerComposeUp } from '../src/docker.ts';
+
+const DOCKER_COMMANDS = { up: dockerComposeUp, down: dockerComposeDown } as const;
+
+const args = process.argv.slice(2);
+
+if (args.length !== 1) {
+    throw new Error(
+        `This script expects exactly one argument, which must be one of the following: ${Object.keys(DOCKER_COMMANDS)}`,
+    );
+}
+
+const testSets: TestSet[] = await getTestSets();
+
+const dockerCommand = getDockerCommand(args[0]);
+
+await Promise.all(testSets.map(dockerCommand));
+
+function getDockerCommand(command: string) {
+    if (!(command in DOCKER_COMMANDS)) {
+        throw new Error(`Invalid command: ${command}, expected one of ${Object.keys(DOCKER_COMMANDS)}`);
+    }
+
+    return DOCKER_COMMANDS[command as keyof typeof DOCKER_COMMANDS];
+}
