c5e6fed7290cdd2061105239161cfe180fc6c7d3
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-10-21 14:25:41 +0200

    refactor: generalize and unify docker scripts

--- src/docker.ts
+++ src/docker.ts
@@ -1,39 +1,61 @@
-import { execSync, exec } from 'child_process';
+import { exec } from 'child_process';
 import { ExecSyncOptions } from 'node:child_process';
 import { promisify } from 'util';
+import { TestSet } from './testSet.js';
+import path from 'path';
+import fs from 'fs';
 
 const PROJECT_PREFIX = 'lapis-silo-e2e-';
 
 const execAsync = promisify(exec);
 
-export function dockerComposeUp(projectName: string, dockerComposeEnv: string[]) {
-    const dockerComposeUpCommand = [
+export function dockerComposeUp(testSet: TestSet) {
+    const projectName = getDockerComposeProjectName(testSet);
+
+    const dataDir = path.join(testSet.path, 'data');
+    const outputDir = path.join(testSet.path, 'output');
+    if (fs.existsSync(outputDir)) {
+        fs.rmSync(outputDir, { recursive: true, force: true });
+    }
+    fs.mkdirSync(outputDir);
+
+    const dockerComposeEnv = [
+        'LAPIS_TAG=latest',
+        'SILO_TAG=latest',
+        `LAPIS_PORT=${testSet.lapisPort}`,
+        `SILO_PORT=${testSet.siloPort}`,
+        `TESTSET_DATA_FOLDER=${dataDir}`,
+        `TESTSET_OUTPUT_FOLDER=${outputDir}`,
+    ];
+
+    return execute([
         ...dockerComposeEnv,
         'docker',
         'compose',
-        `--project-name=${PROJECT_PREFIX}${projectName}`,
+        `--project-name=${projectName}`,
         '--progress=plain',
         'up',
         '--quiet-pull',
         '--detach',
         '--wait',
-    ].join(' ');
-
-    console.log(dockerComposeUpCommand);
-    const execOptions: ExecSyncOptions = process.env.VERBOSE ? { stdio: 'inherit' } : { stdio: 'ignore' };
-    return execAsync(dockerComposeUpCommand, execOptions);
+    ]);
 }
 
-export function dockerComposeDown(testName: string) {
-    const dockerComposeDownCommand = [
-        'docker',
-        'compose',
-        `--project-name=${PROJECT_PREFIX}${testName}`,
-        '--progress=plain',
-        'down',
-    ].join(' ');
+export function dockerComposeDown(testSet: TestSet) {
+    const projectName = getDockerComposeProjectName(testSet);
 
-    console.log(dockerComposeDownCommand);
+    return execute(['docker', 'compose', `--project-name=${projectName}`, '--progress=plain', 'down']);
+}
+
+function getDockerComposeProjectName(testSet: TestSet) {
+    const testBaseDir = path.basename(testSet.path);
+    const testName = testBaseDir.toLowerCase();
+    return `${PROJECT_PREFIX}${testName}`;
+}
+
+function execute(dockerComposeUpCommand: string[]) {
+    const command = dockerComposeUpCommand.join(' ');
+    console.log(command);
     const execOptions: ExecSyncOptions = process.env.VERBOSE ? { stdio: 'inherit' } : { stdio: 'ignore' };
-    execSync(dockerComposeDownCommand, execOptions);
+    return execAsync(command, execOptions);
 }
